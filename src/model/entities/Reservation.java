fcbd8c26a9bed3f312dca91198423dab1a182eb8
ThiagoDesWEb <thiagomooura@gmail.com>
2026-03-25 20:58:46 -0300

    Very bad solution

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,64 @@
+package model.entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Reservation {
+
+	private Integer roomNumber;
+	private LocalDate checkIn;
+	private LocalDate checkOut;
+	
+	private static DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
+	public Reservation() {
+		
+	}
+
+	public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public LocalDate getCheckIn() {
+		return checkIn;
+	}
+
+	public LocalDate getCheckOut() {
+		return checkOut;
+	}
+
+	public long duration() {
+		long diff = ChronoUnit.DAYS.between(checkIn, checkOut);
+		return diff;
+	}
+	
+	public void updateDates(LocalDate checkIn, LocalDate checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room "
+			+ roomNumber
+			+ ", check-in: "
+			+ fmt1.format(checkIn)
+			+ ", check-out: "
+			+fmt1.format(checkOut)
+			+ ", "
+			+ duration()
+			+ " nights";
+	}
+
+}
